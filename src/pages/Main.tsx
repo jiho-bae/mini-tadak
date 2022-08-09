3c67029e1fe4e339fc522ed2b6b47ffb38785735
jiho-bae <jiho@dgu.ac.kr>
2022-08-10 04:46:13 +0900

    Feat: 방 입장 로직 추가 - 룸카드 클릭시 부모에서 버블링된 이벤트 감지 - 클릭한 룸카드의 uuid 기반으로 입장할 방 정보 요청 - 방 정원 초과시(canIEnterRoom) 입장불가 - 방 입장 가능시 ref에 방 정보 저장 - enterClickedRoom 함수로 방 입장

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import RoomCard from '../components/RoomCard';
@@ -14,8 +14,18 @@ export default function Main() {
   const loginUser = location.state as LocationState;
   const [loading, setLoading] = useState(true);
   const [rooms, setRooms] = useState<RoomType[]>([]);
+  const roomDataRef = useRef<RoomType>();
 
-  const getRoom = useCallback(async () => {
+  const redirectLoginPage = useCallback(() => {
+    navigate('/', { replace: true });
+  }, [navigate]);
+
+  const enterClickedRoom = useCallback(() => {
+    const { uuid } = roomDataRef.current as RoomType;
+    navigate(`/room/${uuid}`, { replace: true });
+  }, [navigate]);
+
+  const fetchRoomList = useCallback(async () => {
     const res = await fetch('/api/room?type=타닥타닥&search=&page=1&take=15');
     const resData = await res.json();
     const { results } = resData.data;
@@ -24,9 +34,33 @@ export default function Main() {
     setLoading(false);
   }, []);
 
-  const redirectLoginPage = useCallback(() => {
-    navigate('/', { replace: true });
-  }, [navigate]);
+  const fetchClickedRoomInfoByUuid = useCallback(async (uuid: string) => {
+    const res = await fetch(`/api/room/${uuid}`);
+    const resData = await res.json();
+    const { data } = resData;
+    return data;
+  }, []);
+
+  const canIEnterRoom = useCallback((roomData: RoomType) => {
+    const { nowHeadcount, maxHeadcount } = roomData;
+    return nowHeadcount >= maxHeadcount;
+  }, []);
+
+  const onClickRoomCard = useCallback(
+    async (e: any) => {
+      const roomCard = e.target.closest('.room-card');
+      const { cardIdx } = roomCard.dataset;
+      const { uuid } = rooms[cardIdx];
+      const clickedRoomInfo = await fetchClickedRoomInfoByUuid(uuid);
+      if (!canIEnterRoom(clickedRoomInfo)) {
+        return;
+      }
+
+      roomDataRef.current = clickedRoomInfo;
+      enterClickedRoom();
+    },
+    [rooms, fetchClickedRoomInfoByUuid, canIEnterRoom, enterClickedRoom],
+  );
 
   useEffect(() => {
     if (!loginUser) {
@@ -36,15 +70,15 @@ export default function Main() {
   }, [loginUser, navigate, redirectLoginPage]);
 
   useEffect(() => {
-    getRoom();
-  }, [getRoom]);
+    fetchRoomList();
+  }, [fetchRoomList]);
 
   if (loading) return <div>loading...</div>;
 
   return (
     <main>
       <Header onClickLogOut={redirectLoginPage} userId={loginUser.userId} />
-      <section className="pack gap(10)">
+      <section className="pack gap(10)" onClick={onClickRoomCard}>
         {rooms.map((room, idx) => (
           <RoomCard key={room.id} cardIdx={idx} {...room} />
         ))}
