8361a7f7e1c78fbf3dfac8f49a8c6f6b8997f2f1
jiho-bae <jiho@dgu.ac.kr>
2022-08-09 20:08:43 +0900

    Feat: 타입 추가

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+import { DevFieldType } from './devField';
+import RoomType from './room';
+import UserType from './user';
+
+export type { DevFieldType, RoomType, UserType };
---
+++ src/types/room.ts
@@ -0,0 +1,16 @@
+import UserType from './user';
+
+export default interface RoomType {
+  id: number;
+  agoraAppId: string;
+  agoraToken: string;
+  createdAt: string;
+  description: string;
+  nowHeadcount: number;
+  maxHeadcount: number;
+  owner?: UserType;
+  title: string;
+  roomType: string;
+  updatedAt: string;
+  uuid: string;
+}
