22cf50fd79db06a77f2571aaf98cef63227fe7f2
jiho-bae <jiho@dgu.ac.kr>
2022-08-09 19:56:05 +0900

    Feat: 로그인 페이지 생성 - 아이디 입력 후 확인을 누르면 main 페이지로 이동 - 아이디를 입력하지 않으면 alert을 띄운다.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
 type LoginProps = {};
 
 export default function Login({}: LoginProps) {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [userId, setUserId] = useState('');
+
+  const onChangeUserId = (e: React.ChangeEvent<HTMLInputElement>) => setUserId(e.target.value);
+
+  const isEmptyUserId = (userId: string) => {
+    if (userId === '') {
+      alert('아이디를 입력하세요.');
+      return true;
+    }
+    return false;
+  };
+
+  const goToMain = (userId: string) => {
+    navigate('/main', { state: { userId } });
+  };
+
+  const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isEmptyUserId(userId)) {
+      return;
+    }
+
+    goToMain(userId);
+  };
+
+  return (
+    <div className="@w(~375):w(150) @w(376~):w(360) h(200)  m(auto/auto) p(15) bg(white) r(10) vbox(center) gap(80)">
+      <h1 className="font(24) bold">미니타닥 로그인</h1>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="user_id">아이디</label>
+        <input value={userId} onChange={onChangeUserId} id="user_id" className="m(0/5/0/5)" maxLength={12} />
+        <button className="p(5) r(5) bg(#ff6348) hover:bg(#70a1ff) c(white)">로그인</button>
+      </form>
+    </div>
+  );
 }
