67fb13ffbc3f263f5c232fced19f5d130c86c647
jiho-bae <jiho@dgu.ac.kr>
2022-08-10 13:53:55 +0900

    Add: 코드 가져오기 기존 프로젝트에서 유틸코드 등 가져왔음

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,13 @@
+import React, { useState, useCallback } from 'react';
+
+export default function useInput(initialState: string): [string, typeof onChange, typeof onReset] {
+  const [input, setInput] = useState(initialState);
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setInput(e.target.value),
+    [],
+  );
+  const onReset = useCallback(() => setInput(''), []);
+
+  return [input, onChange, onReset];
+}
