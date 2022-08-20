4a42106de3762a1ecb7f74aba6408f5ad87e5122
jiho-bae <jiho@dgu.ac.kr>
2022-08-21 03:28:44 +0900

    Feat: intersectionobserver, debounce 적용 - 마지막 방요소가 보이기 전에 intersectionobserver를 통해 방목록 추가요청

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,6 +5,8 @@ import { useRecoilState } from 'recoil';
 import Header from 'src/components/Header';
 import RoomCard from 'src/components/RoomCard';
 import Tab from 'src/components/common/Tab';
+import Loader from 'src/components/Loader';
+import SearchBar from 'src/components/SearchBar';
 
 import { userState } from 'src/hooks/recoil/user/atom';
 import { useToast } from 'src/hooks/useToast';
@@ -12,14 +14,16 @@ import { enterRoom, redirectPage } from 'src/utils/history';
 import { ErrorResponse, RoomType } from 'src/types';
 import { auth } from 'src/apis/auth';
 import { getRoom, getRoomByUuid, postLogout, ResponseGetRoomData } from 'src/apis';
-import { TOAST_MESSAGE } from 'src/utils/constant';
+import { INFINITE_SCROLL, TOAST_MESSAGE } from 'src/utils/constant';
 import afterFetcher from 'src/apis/afterFetcher';
 import { TAB_NAME } from 'src/utils/constant';
 import { getRoomQueryObj } from 'src/apis/apiUtils';
 import useInput from 'src/hooks/useInput';
-import SearchBar from 'src/components/SearchBar';
+import useDebounce from 'src/hooks/useDebounce';
 
 const tabWrapperStyle = 'pack mt(1.8rem) w(100%) relative mb(1.8rem)';
+
+let roomObserver: IntersectionObserver;
 
 export default function Main() {
   const navigate = useNavigate();
@@ -27,9 +31,11 @@ export default function Main() {
   const [rooms, setRooms] = useState<RoomType[]>([]);
   const [tabState, setTabState] = useState({ minitadak: true, campfire: false });
   const [searchStr, onChangeSearch, onResetSearch] = useInput('');
+  const debounceSearch = useDebounce({ value: searchStr });
   const [user, setUser] = useRecoilState(userState);
-  const currentPage = useRef(1);
   const { errorToast } = useToast();
+  const currentPage = useRef(1);
+  const endRef = useRef<HTMLDivElement>(null);
 
   const onClickMiniTadakTab = () => setTabState({ minitadak: true, campfire: false });
   const onClickCampfireTab = () => setTabState({ minitadak: false, campfire: true });
@@ -57,7 +63,6 @@ export default function Main() {
         onSuccess: (data: ResponseGetRoomData) => {
           if (page === 1) setRooms([...data.results]);
           else setRooms((preRooms) => [...preRooms, ...data.results]);
-          currentPage.current += 1;
 
           setLoading(false);
         },
@@ -100,6 +105,11 @@ export default function Main() {
     [rooms, canIEnterRoom, errorToast, navigate],
   );
 
+  const addNewPage = useCallback(() => {
+    currentPage.current += 1;
+    getRoomList(debounceSearch);
+  }, [getRoomList, debounceSearch]);
+
   useEffect(() => {
     if (!auth.hasAccessToken()) {
       redirectLoginPage();
@@ -113,6 +123,34 @@ export default function Main() {
     }
   }, [user, getRoomList]);
 
+  useEffect(() => {
+    if (endRef.current?.lastElementChild) {
+      const { lastElementChild } = endRef.current;
+
+      roomObserver = new IntersectionObserver(
+        function (entries, observer) {
+          entries.forEach(function (entry) {
+            if (entry.isIntersecting && rooms.length === currentPage.current * INFINITE_SCROLL.unit) {
+              console.log('good');
+              observer.unobserve(entry.target);
+              addNewPage();
+            }
+          });
+        },
+        {
+          threshold: INFINITE_SCROLL.threshold,
+        },
+      );
+      roomObserver.observe(lastElementChild);
+    }
+
+    return () => roomObserver.disconnect();
+  }, [addNewPage, rooms]);
+
+  useEffect(() => {
+    currentPage.current = 1;
+  }, [tabState]);
+
   return (
     <main className="vbox w(80%)">
       <Header onClickLogOut={onClickLogOut} userId={user.nickname} />
@@ -122,13 +160,20 @@ export default function Main() {
         <div className="w(10)" />
         <SearchBar search={searchStr} onChange={onChangeSearch} onReset={onResetSearch} />
       </div>
-      <section className="h(70vh) scroll no-scrollbar hbox gap(10) flex-wrap @w(~450):vpack" onClick={onClickRoomCard}>
-        {rooms.map((room, idx) => (
-          <RoomCard key={room.id} cardIdx={idx} {...room} />
-        ))}
-        {!loading && (
+      <section
+        ref={endRef}
+        className="h(70vh) scroll no-scrollbar hbox gap(30) flex-wrap @w(~450):vpack"
+        onClick={onClickRoomCard}>
+        {rooms.length ? (
+          rooms.map((room, idx) => <RoomCard key={room.id} cardIdx={idx} {...room} />)
+        ) : (
+          <div className="w(100%) pack">
+            <h2 className="font(2.2rem) c(rgba(0,0,0,0.6))">개설된 방이 없습니다. 😭</h2>
+          </div>
+        )}
+        {loading && (
           <div className="w(100%) pack mt(10)">
-            <div>loading......</div>
+            <Loader displayText="방을 불러오는 중입니다.." />
           </div>
         )}
       </section>
